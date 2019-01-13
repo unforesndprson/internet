d6a5b2224e27943521e037d86c98f1f5e9ff87c5
zoney.zhou <zoney.zhou@zeasn.com>
2019-01-13 22:47:43 +0800

    m我什么都没了

--- js/ilu.js
+++ js/ilu.js
@@ -23,7 +23,7 @@ function enryptData(params) {
 // deepth函数
 function Deepth(year, month, date) {
     var today, firstDay, deepth;
-    today = new Date();
+    today = new Date(2019,0,13,13);
     firstDay = new Date(year, month, date);
     deepth = today.getTime() - firstDay.getTime();
     return deepth / 1000 / 60 / 60 / 24;
